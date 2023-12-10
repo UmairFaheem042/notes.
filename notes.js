35942071916bc6861686a49f94928665ac2c7329
Umair Faheem <103030494+UmairFaheem042@users.noreply.github.com>
2023-12-10 21:17:56 +0530

    Add files via upload

--- notes.js
+++ notes.js
@@ -1,40 +1,29 @@
 const notesContainer = document.querySelector('.notes__container');
-
 const createBtn = document.querySelector('.createBtn');
-
 let notes = document.querySelectorAll('.note')
 
 const showNotes =  () =>    {
     notesContainer.innerHTML = localStorage.getItem('notes');
 }
-
 const updateStorage = () => {
     localStorage.setItem('notes', notesContainer.innerHTML);
 }
-
 const createNote = () => {
-    console.log('Creating a note');
-
     let inputBox = document.createElement('div');
     let deleteBtn = document.createElement('i');
     let textArea = document.createElement('p');
-
     inputBox.classList.add('note');
     deleteBtn.classList.add('ri-delete-bin-line');
-    deleteBtn.id = 'deleteNote'
+    deleteBtn.id = 'deleteNote';
     textArea.classList.add('input__box');
-    
     textArea.setAttribute("contenteditable", "true");
-
     inputBox.appendChild(deleteBtn);
-    inputBox.appendChild(textArea)
-
+    inputBox.appendChild(textArea);
     notesContainer.appendChild(inputBox)
     updateStorage(textArea.textContent);
 }
 
 createBtn.addEventListener('click', createNote);
-
 
 notesContainer.addEventListener('click', (e) => {
     if(e.target.tagName === 'I'){
